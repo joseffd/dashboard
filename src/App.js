8ee30856bc1cb8f063c7255edadb3a709752559b
Joseff Davies <joseff.davies@mkodo.com>
2020-12-22 10:04:24 +0000

    login form setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { Route, Switch } from "react-router-dom";
-import { LoginPage } from "./login/LoginPage";
+import  LoginPage  from "./login/LoginPage";
 
 function App() {
   return (
