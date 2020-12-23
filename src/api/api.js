9f87e9f63c913d97b59fa01cb1445d0a815b1adb
Joseff Davies <joseff.davies@mkodo.com>
2020-12-23 17:06:26 +0000

    added geolocation

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,11 @@
 import {handleError, handleResponse} from "./apiUtils";
 
 const APIkey = 'd0a10211ea3d36b0a6423a104782130e';
-let lat = 51.5074;
-let lon = -0.1278;
 
-export function getWeather(){
+
+export function getWeather(coord){
+    let lat = coord.lat;
+    let lon = coord.lon;
     return fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIkey}`).then(handleResponse).catch(handleError);
 }
 
--- src/weather/weatherWidget.js
+++ src/weather/weatherWidget.js
@@ -4,18 +4,19 @@ import { WiDaySunny, WiCloudy, WiRain } from 'weather-icons-react';
 
 export default function WeatherWidget() {
     const [weatherData, setWeatherData] = useState({loading: true, data: null,});
+    const [coord, setCoords] = useState({lat: 0, lon: 0});
 
     navigator.geolocation.getCurrentPosition(function(position) {
-        console.log("Latitude is :", position.coords.latitude);
-        console.log("Longitude is :", position.coords.longitude);
+        setCoords({lat: position.coords.latitude, lon: position.coords.longitude});
     });
+
+
     useEffect(() => {
     setWeatherData({loading: true});
-        getWeather().then((data) => {
+        getWeather(coord).then((data) => {
             setWeatherData({loading:false, data})
         });
     }, [setWeatherData]);
-    console.log(weatherData.data)
     return(
         <>
             <div className={"title"}><h1>Weather</h1></div>
@@ -33,11 +34,11 @@ function icon(weather){
     console.log(weather);
     switch(weather){
         case "Clouds":
-            return <WiCloudy size={24} color='#000' />;
+            return <WiCloudy size={50} color='#000' />;
         case "Sun":
-            return <WiDaySunny size={24} color='#000' />;
+            return <WiDaySunny size={50} color='#000' />;
         case "Rain":
-            return <WiRain size={24} color='#000' />;
+            return <WiRain size={50} color='#000' />;
         default:
             return weather;
     }
